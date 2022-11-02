be96673cbc87dc919b8c35785758a84d48671887
ManojPandi001 <manojkumarpandit.mk@gmail.com>
2022-11-02 12:52:16 +0530

    I have fixed it

--- prime.js
+++ prime.js
@@ -1,11 +1,18 @@
 function myfunction(num){
+    let count=0;
 for(let i = 1;i<=num;i++){
 if(num%i==0){
-return true;	
-} else{
-return false;	
+    count++;	
 }
+}
+if(count==2){
+    return true;
 }	
 }
 let ans = myfunction(13);
-console.log(ans);
+
+if(ans==true){
+    console.log("Prime")
+}else{
+    console.log("No Prime")
+}
